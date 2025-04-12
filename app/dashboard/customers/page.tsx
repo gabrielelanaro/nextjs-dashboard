5d0d203fb6261c38a8cfda3f4cce8f6d93f88d58
Gabriele Lanaro <gabriele.lanaro@gmail.com>
2025-04-12 16:44:31 +0200

    Update codebase

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Page: React.FC = () => {
+    return <p>Customers page</p>;
+};
+
+export default Page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Page: React.FC = () => {
+    return <p>Invoices page</p>;
+};
+
+export default Page;
